1ff2839e09229bf1cc24fd69f5930452555621a7
mfcpecasservicos <mfcpecasservicos@gmail.com>
2026-05-28 11:58:33 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'mfc-tecnico-v7';
-const APP_SHELL = [
+const CACHE_NAME='mfc-tecnico-v6';
+const APP_SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
@@ -8,44 +8,20 @@ const APP_SHELL = [
   './mfc-logo.png',
   './mfc-capa.png'
 ];
-
-self.addEventListener('install', event => {
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL).catch(()=>null)));
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL).catch(() => null))
-  );
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
-      .then(() => self.clients.claim())
-  );
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+  self.clients.claim();
 });
-
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  if (req.method !== 'GET') return;
-
-  const url = new URL(req.url);
-  if (url.origin !== location.origin) return;
-
-  if (req.mode === 'navigate') {
-    event.respondWith(
-      fetch(req).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy)).catch(() => {});
-        return res;
-      }).catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    fetch(req).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => {});
-      return res;
-    }).catch(() => caches.match(req))
-  );
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET')return;
+  event.respondWith(fetch(req).then(res=>{
+    const copy=res.clone();
+    caches.open(CACHE_NAME).then(cache=>cache.put(req,copy)).catch(()=>{});
+    return res;
+  }).catch(()=>caches.match(req).then(cached=>cached||caches.match('./index.html'))));
 });
