a678fd0885852029683880776ac6c8bf600dbc09
mfcpecasservicos <mfcpecasservicos@gmail.com>
2026-06-09 15:39:21 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mfc-v12';
+const CACHE_NAME = 'mfc-v13';
 const APP_SHELL = ['./','./index.html','./cadastro.html','./gestor.html','./manifest.webmanifest','./mfc-logo.png','./mfc-capa.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL).catch(()=>null))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(()=>self.clients.claim())); });
