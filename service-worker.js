38e0d5cb3001fbe9710365be7e0aa7e42a3fbcf8
mfcpecasservicos <mfcpecasservicos@gmail.com>
2026-08-13 15:07:22 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ messaging.onBackgroundMessage(payload=>{
   self.registration.showNotification(title,{body,icon:'./icon-192.png',badge:'./icon-192.png'});
 });
 
-const CACHE_NAME = 'mfc-tecnico-v112';
+const CACHE_NAME = 'mfc-tecnico-v115';
 const APP_SHELL = [
   './',
   './index.html',
