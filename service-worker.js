3908fda5da92225b1e9d9eb3b78ac6b59a6e5924
mfcpecasservicos <mfcpecasservicos@gmail.com>
2026-08-24 11:36:57 -0300

    Atualiza cache para v137d

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ messaging.onBackgroundMessage(payload=>{
   self.registration.showNotification(title,{body,icon:'./icon-192.png',badge:'./icon-192.png'});
 });
 
-const CACHE_NAME = 'mfc-tecnico-v137c';
+const CACHE_NAME = 'mfc-tecnico-v137d';
 const FIREBASE_MODULES = [
   'https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js',
   'https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js',
