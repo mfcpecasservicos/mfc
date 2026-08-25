06bc9770fca676246b2697c063c0571edf8ff4e5
mfcpecasservicos <mfcpecasservicos@gmail.com>
2026-08-25 15:24:15 -0300

    v138: alinhar cache do modulo de midia

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ const APP_SHELL = [
   './cadastro.html',
   './layout-guide.js',
   './report-media.js',
-  './report-media.js?v=137o',
+  './report-media.js?v=138',
   './location-view.js',
   './vendor/leaflet/leaflet.js',
   './vendor/leaflet/leaflet.css',
