509c123cb7b8cf1c77fcf1042a085a03de05c566
mfcpecasservicos <mfcpecasservicos@gmail.com>
2026-08-25 15:12:30 -0300

    v137o: atualizar cache do modulo de midia

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ const APP_SHELL = [
   './cadastro.html',
   './layout-guide.js',
   './report-media.js',
-  './report-media.js?v=137n',
+  './report-media.js?v=137o',
   './location-view.js',
   './vendor/leaflet/leaflet.js',
   './vendor/leaflet/leaflet.css',
